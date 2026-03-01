32b0bc6eaf787a55f8bc914bbd2de55f717265d1
trannguyenanhkhoa0104@gmail.com <trannguyenanhkhoa0104@gmail.com>
2026-03-01 22:25:10 +0700

    doc: api

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
+    unoptimized: true,
     remotePatterns: [
       { protocol: "https", hostname: "placehold.co" },
       { protocol: "https", hostname: "m.media-amazon.com", pathname: "/**" },
